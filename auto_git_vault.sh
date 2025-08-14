19df9e4233f82245593f52e9cc066dad3ec03243
Kayla Phan <kaylaphanmaianh@gmail.com>
2025-08-14 15:47:08 -0400

    commited: 2025-08-14 15:47:07

--- auto_git_vault.sh
+++ auto_git_vault.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-# Change to the folder where this script lives
 cd "$(dirname "$0")"
 
-# Pull latest changes
 git pull origin main
 
-# Stage all changes
 git add .
 
-# Commit with timestamp
 git commit -m "commited: $(date +"%Y-%m-%d %H:%M:%S")"
 
-# Push to GitHub
 git push origin main
 
